3a248ef82e318f94e9558cf89f357c5e374def43
siputzx <159487561+siputzx@users.noreply.github.com>
2025-11-07 11:02:13 +0700

    Update entrypoint.sh

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-cd /home/container
+cd /home/container || exit 1
 
-# Make internal Docker IP address available to processes.
+# Set timezone
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Print Node.js Version
+# Print Node.js version
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mnode -v\n"
 node -v
 
-# Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+# Print npm version  
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mnpm -v\n"
+npm -v
 
-# Run the Server
-eval ${MODIFIED_STARTUP}
+# Execute the startup command
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$STARTUP"
+eval ${STARTUP}
