fbab48b9b51c1a78b6e7fc49fc173053343e607c
siputzx <siputzz.id@gmail.com>
2026-04-03 17:40:15 +0700

    Ensure user-local bin is on shell PATH

--- universal/debian/tokoptero-shell.sh
+++ universal/debian/tokoptero-shell.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export PATH="${HOME}/.local/bin:/home/container/.local/bin:${PATH}"
 export HOSTNAME="tokoptero"
 export PS1='\[\e[1;32m\]\u@tokoptero\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ '
 
